1ad07d068ee7e2f9f1202278e4ef8e0bccc6fb5d
cristianfchq <cristian.fchq@gmail.com>
2020-01-14 11:00:19 -0400

    una vez guardada las tareas mostra en el HTML en un div las tareas almacenadas en el localStorage y se puede volver a agregar nuevas tareas

--- js/app.js
+++ js/app.js
@@ -45,10 +45,41 @@ function saveTask(e){
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
     //!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!/!
+
+    getTasks();
+
+
     e.preventDefault();
 }
 
-// function getTasks(){
-//     let tasks = JSON.parse(localStorage.getItem('tasks'));
-//     let tasksView = document.getElementById('tasks');
-// }
+function getTasks(){
+
+    // let title = document.getElementById('title').value;
+    // let description = document.getElementById('description').value;
+
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    let tasksView = document.getElementById('tasks');//este tasks es el id dentro del div en HTML
+
+    // var element = document.createElement('div');
+
+    tasksView.innerHTML = '';
+
+    for(let i = 0; i < tasks.length; i++) {
+        // console.log(tasks[i]);
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+        tasksView.innerHTML += `
+        <div class="card mb-3">
+            <div class="card-body">
+                <p>${title} - ${description}</p>
+                <a class="btn btn-danger">
+                    Delete
+                </a>
+            </div>
+        </div>
+        `;
+    }
+}
+
+// getTasks(); 
